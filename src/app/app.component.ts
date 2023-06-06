f92bcbe214d0b844bbf46e9407ecad3e81c930a3
tienbku <tien.ngtruong@gmail.com>
2023-06-06 10:41:58 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-16-image-upload-preview';
+  title = 'Angular 16 Image upload with Preview';
 }
